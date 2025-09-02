5821eda8565cf65d1cf8dbc4f063f4723fc133ac
andrews <andrewswayne21@gmail.com>
2025-09-02 16:01:00 +0000

    github and netlify

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,16 @@ import react from '@vitejs/plugin-react'
 // export default defineConfig({
 //   plugins: [react()],
 // })
+// export default defineConfig({
+//   plugins: [react()],
+//   base: '/my-portfolio-website/', //  must match your repo name
+// })
+
+// If deploying to GitHub Pages, set this environment variable before building
+const isGithubPages = process.env.GH_PAGES === 'true'
+
 export default defineConfig({
   plugins: [react()],
-  base: '/my-portfolio-website/', //  must match your repo name
+  base: isGithubPages ? '/my-portfolio-website/' : '/', 
 })
+
